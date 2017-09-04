e86e5ecb3afb11443a4a4e4aeff32f05293d9902
mnsekh111@gmail.com <mnsekh111@gmail.com>
2017-09-04 16:21:42 -0700

    Add footer template

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer dark fixed class="primary">
+    <v-spacer></v-spacer>
+    <div>© {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
