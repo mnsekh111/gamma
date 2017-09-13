40aff004fbbba7a3c157e7295121b285d6299319
smnatara <smnatara@ncsu.edu>
2017-09-12 22:11:20 -0700

    change style of gamma card

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,18 +1,54 @@
 <template>
   <div>
     <v-container>
-      <v-card>
+      <v-card class="elevation-12">
         <v-card-title primary-title>
           <div>
+            <v-icon class="display-4">desktop_windows</v-icon>
             <h3 class="headline mb-0">{{display.name}}</h3>
           </div>
         </v-card-title>
         <v-card-text>
           <v-layout row wrap>
-            <v-flex xs3>
-              <v-slider label="Red" v-model="display.gamma.red" thumb-label min="10" max="100"></v-slider>
-              <v-slider label="Green" v-model="display.gamma.green" thumb-label min="10" max="100"></v-slider>
-              <v-slider label="Blue" v-model="display.gamma.blue" thumb-label min="10" max="100"></v-slider>
+            <v-flex xs12 sm6>
+              <v-card class="pa-3 ma-3">
+                <h3 class="title">Gamma</h3>
+                <v-layout row wrap>
+                  <v-flex xs12 sm2>
+                    <v-icon class="red--text">lightbulb_outline</v-icon>
+                  </v-flex>
+                  <v-flex xs12 sm8>
+                    <v-slider v-model="display.gamma.red" thumb-label min="10" max="100"></v-slider>
+                  </v-flex>
+                  <v-flex xs12 sm2>
+                    <div class="red--text body-2 text-xs-center"> ({{display.gamma.red}}) </div>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row wrap>
+                  <v-flex xs12 sm2>
+                    <v-icon class="green--text">lightbulb_outline</v-icon>
+                  </v-flex>
+                  <v-flex xs12 sm8>
+                    <v-slider v-model="display.gamma.green" thumb-label min="10" max="100"></v-slider>
+                  </v-flex>
+                  <v-flex xs12 sm2>
+                    <div class="green--text body-2 text-xs-center"> ({{display.gamma.green}}) </div>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row wrap>
+                  <v-flex xs12 sm2>
+                    <v-icon class="blue--text">lightbulb_outline</v-icon>
+                  </v-flex>
+                  <v-flex xs12 sm8>
+                    <v-slider v-model="display.gamma.blue" thumb-label min="10" max="100"></v-slider>
+                  </v-flex>
+                  <v-flex xs12 sm2>
+                    <div class="blue--text body-2 text-xs-center"> ({{display.gamma.blue}}) </div>
+                  </v-flex>
+                </v-layout>
+              </v-card>
             </v-flex>
           </v-layout>
         </v-card-text>
@@ -43,3 +79,9 @@
     }
   }
 </script>
+<style scoped>
+  .headline {
+    display: inline-block;
+    padding-left: 10px;
+  }
+</style>
